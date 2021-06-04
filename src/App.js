ba508e5c59922a79eb089e11c67080bc7e2742fa
Utkarsh Katiyar <utkarsh_katiyar@outlook.com>
2021-06-05 00:10:56 +0530

    Project uploaded from local

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Burger from "./components/Burger";
+
+function App() {
+  return (
+    <div className="burgerContent">
+      <Burger />
+    </div>
+  );
+}
+
+export default App;
